d5c677dcb9a870177e4e610a7fbcd7fa3e4c030b
Aquarelix <AquarelixOG@protonmail.com>
2022-06-10 15:27:18 +0200

    Initial Commit

---
+++ src/components/ClickerComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Clicker</h1>
+    <img src="../assets/logo.png" alt="Clicker Image" @click="incrementCounter" class="clickerImage">
+</template>
+
+<script>
+
+export default({
+    setup() {
+        
+    },
+    methods: {
+        incrementCounter(){
+            this.$store.commit('increment')
+        }
+    }
+})
+</script>
+
+<style scoped>
+.clickerImage{
+    border: 2px solid black;
+    border-radius: 1rem;
+}
+</style>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="ClickerComponent">
+      <ClickerComponentVue />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import ClickerComponentVue from '@/components/ClickerComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    ClickerComponentVue
+  }
+}
+</script>
+
+<!-- Scoped: you will only style the component-->
+<style scoped>
+  .ClickerComponent{
+    background-color: aqua;
+  }
+</style>
