b3f5f38a81924627e1bc1400fcb100ad87b6528a
weidner <henrik.weidner@hs-weingarten.de>
2022-06-27 22:17:54 +0200

    Fixxed the pain

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,18 @@ export default {
       this.$router.push("/login")
 
     this.$store.dispatch('resetAllVariables')
+    
+    // Set the monster intervall
+    if (this.$store.getters.getMonsterInterval == null)
+    {
+        var monsterIntervall = setInterval(() => {
+            const count = Number(this.$store.state.count)
+            const counterRate = Number(this.$store.state.counterRate)
+            this.$store.state.count = count + counterRate
+        }, 1000)
+        this.$store.dispatch('setMonsterIntervall', monsterIntervall)
+    }
+
   },
   methods: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,18 +39,6 @@ export default {
         }
         this.SaveUserStatsOnDatabase()
       }, 30000);
-
-    // Set the monster intervall
-    if (this.$store.getters.getMonsterInterval == null)
-    {
-        var monsterIntervall = setInterval(() => {
-            const count = Number(this.$store.state.count)
-            const counterRate = Number(this.$store.state.counterRate)
-            this.$store.state.count = count + counterRate
-        }, 1000)
-        this.$store.dispatch('setMonsterIntervall', monsterIntervall)
-    }
-    
     this.LoadUserStats()
   },
   beforeUnmount(){
